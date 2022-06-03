881197d3243559f3ef3c6b1fb48aadb719e39254
comdiv <fagim.sadykov@gmail.com>
2022-06-03 12:06:54 +0500

    cleanNonBlockedPixels2C - comdiv solution 1

--- src/main/kotlin/codes/sepctrum/conf2022/images/task/cleanNonBlockedPixels.kt
+++ src/main/kotlin/codes/sepctrum/conf2022/images/task/cleanNonBlockedPixels.kt
@@ -21,5 +21,70 @@ fun cleanNonBlockedPixels2C(
     foreground: Color = Color.BLACK,
     background: Color = Color.WHITE,
 ) {
-    //TODO
+    // кэш пикселей, которые могут быть началом блока
+    val blockStarters = LinkedList<Pair<Int, Int>>()
+    val fg = foreground.rgb
+    val bg = background.rgb
+    // обходим по строкам
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            val pixel = image[x, y]
+            if (pixel != fg) {
+                continue
+            }
+            // проверим не является ли точка началом блока
+            var isBlockStarter = false
+            // только если блок вообще еще помещается
+            if (x + minwidth <= image.width && y + minheight <= image.height) {
+                // только если диагональная точка тоже "черная"
+                if (image[x + minwidth - 1, y + minheight - 1] == fg) {
+                    // сразу предположим что это блок
+                    isBlockStarter = true
+                    // то проверим что все точки в этом прямоугольнике черные
+                    for (bx in x until x + minwidth) {
+                        for (by in y until y + minheight) {
+                            val bpx = image[bx, by]
+                            if(bpx != fg){
+                                isBlockStarter = false
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            // точка точно в блоке если она начинает этот блок
+            if (isBlockStarter) {
+                // занесем точку в реестр точек начал блока
+                // причем всегда ставим в начало - так как нам их обходить надо будет потом в обратном порядке
+                blockStarters.addFirst(x to y)
+                continue
+            }
+
+            var isInBlock = false
+            // также точка в блоке - если есть блок, который ее содержит
+            // обходить выгодно от последнего и тут лучше обходить индексами обратно,
+            // а не создавать
+            for(blk in blockStarters) {
+                // если уже не проходим по вертикальной части
+                // то все, из цикла выходим сразу
+                if(blk.second + minheight - 1 < y) {
+                    break
+                }
+                if(
+                    x >= blk.first && (blk.first + minwidth) > x
+                ) {
+                    isInBlock = true
+                    break
+                }
+            }
+
+            // и вот если точка и не в блоке и не не начало блока, то собственно переводим в фоновый цвет
+            if (!isInBlock) {
+                image[x, y] = bg
+            }
+        }
+    }
 }
